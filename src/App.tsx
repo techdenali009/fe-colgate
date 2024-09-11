dd72931c52f8a8b8a4dd2dddd86adece4001ad17
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-09-11 11:59:27 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline text-blue md:text-gray hover:text-yellow ">
+        Hello Developer!
+      </h1>
+      <h2 className="pt-1">React POC</h2>
+     
+    </>
+  );
+}
+
+export default App;
