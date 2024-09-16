ce83a8c672d79275a718821f55d68ef8f79da9f5
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-09-16 15:13:06 +0530

    routingconfig

---
+++ src/modules/mainModule/mainModule.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+
+// App level Module
+export default function MainModule() {
+    return <><Outlet /></>
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,34 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
-const LandingPage = lazy(()=> import('../pages/LandingPage/index'))
+// Main Module
+const MainModule = lazy(() => import('../modules/mainModule/mainModule'))
+const LandingPage = lazy(() => import('../pages/LandingPage/index'));
+
+// UserModule
+
+// ProductModule
 
 export const AppRoutes = () => {
     return <>
         <Routes>
-            <Route path="/" element={<LandingPage />} />
+            {/* <Route path="/" element={<LoginPage />} /> */}
+            {/* <Route path="/" element={<RegisterPage />} /> */}
+            <Route path="/" element={<MainModule />}>
+                <Route path="" element={<LandingPage />}></Route>
+
+                {/* User Module */}
+                {/* <Route path="/users" element={<UserModule />}>
+                   <Route  path="/" element={<Users/>}></Route>
+                   <Route  path="users/:id" element={<UserDetails}></Route>
+                </Route> */}
+
+                {/* Product module */}
+                {/* <Route path="/products" element={<ProductModule />}>
+                   <Route  path="/" element={<Products/>}></Route>
+                   <Route  path="product/:id" element={<ProductsDetails/>}></Route>
+                </Route> */}
+            </Route>
         </Routes>
     </>
 }
