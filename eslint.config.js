769e395644063d622b27591775ec78cf3f294792
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-09-18 16:48:29 +0530

    scss changes

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,10 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
+    
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
