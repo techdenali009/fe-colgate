af55e0e58aaa476d54adca69eb7b68be5fda1d7f
Prathibha Chavali <prathibha.chavali@techdenali.com>
2024-09-23 20:11:05 +0530

    background color chnaged

--- src/libs/ui/src/organisms/Carousel/index.tsx
+++ src/libs/ui/src/organisms/Carousel/index.tsx
@@ -1,61 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel as ReactCarousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { BannerSlide } from '../Carousel_BannerSlide';
+import { BannerSlide } from '../CarouselBannerSlide';
 import { CarouselProps } from '@utils/interface';
-import './carousel.style.scss'
-
+import './carousel.style.scss';
 
 export const Carousel: React.FC<CarouselProps> = ({ slides }) => {
-  
-  
-  const customPrevArrow = (onClickHandler: () => void, hasPrev: boolean, label: string) => {
-    return (
-      <button
-        type='button'
-        onClick={onClickHandler}
-        className='swiper-button-prev absolute left-0 z-10 p-2 focus:outline-none'
-        aria-label={label}
-      >
-        
-      </button>
-    );
-  };
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const customNextArrow = (onClickHandler: () => void, hasNext: boolean, label: string) => {
-    return (
-      <button
-        type='button'
-        onClick={onClickHandler}
-        className='swiper-button-next  right-0 z-10 p-2 focus:outline-none'
-        aria-label={label}
-      >
-        
-      </button>
-    );
-  };
+    const onChange = (index: number) => {
+        setCurrentIndex(index);
+    };
 
-  return (
-    <ReactCarousel
-      autoPlay
-      infiniteLoop
-      showThumbs={false}
-      showStatus={false}
-      interval={3000}
-      className='carousel-container'
-      renderArrowPrev={customPrevArrow}  
-      renderArrowNext={customNextArrow} 
-    >
-      {slides.map((slide, index) => (
-        <BannerSlide
-          key={index}
-          imgSrc={slide.imgSrc}
-          imgAlt={slide.imgAlt}
-          heading={slide.heading}
-          subheading={slide.subheading}
-          buttonText={slide.buttonText}
-        />
-      ))}
-    </ReactCarousel>
-  );
+    const renderPrevArrow = (onClickHandler: () => void, hasPrev: boolean, label: string) => {
+        const currentSlide = slides[currentIndex];
+        if (currentSlide.bgColor.includes('bg-black')) {
+            return (
+                <button
+                    type="button"
+                    onClick={onClickHandler}
+                    className="custom-prev-arrow-black"
+                    aria-label={label}
+                />
+            );
+        } else if (currentSlide.bgColor.includes('bg-gray-100')) {
+            return (
+                <button
+                    type="button"
+                    onClick={onClickHandler}
+                    className="custom-prev-arrow-other"
+                    aria-label={label}
+                />
+            );
+        }
+        return null; // Fallback if no condition matches
+    };
+
+    const renderNextArrow = (onClickHandler: () => void, hasNext: boolean, label: string) => {
+        const currentSlide = slides[currentIndex];
+        if (currentSlide.bgColor.includes('bg-black')) {
+            return (
+                <button
+                    type="button"
+                    onClick={onClickHandler}
+                    className="custom-next-arrow-black"
+                    aria-label={label}
+                />
+            );
+        } else if (currentSlide.bgColor.includes('bg-gray-100')) {
+            return (
+                <button
+                    type="button"
+                    onClick={onClickHandler}
+                    className="custom-next-arrow-other"
+                    aria-label={label}
+                />
+            );
+        }
+        return null; // Fallback if no condition matches
+    };
+
+    return (
+        <ReactCarousel
+            // autoPlay
+            infiniteLoop
+            showThumbs={false}
+            showStatus={false}
+            interval={3000}
+            className="carousel-container"
+            onChange={onChange} // Update current index on slide change
+            renderArrowPrev={renderPrevArrow}
+            renderArrowNext={renderNextArrow}
+        >
+            {slides.map((slide, index) => (
+                <BannerSlide
+                    key={index}
+                    imgSrc={slide.imgSrc}
+                    imgAlt={slide.imgAlt}
+                    heading={slide.heading}
+                    subheading={slide.subheading}
+                    buttonText={slide.buttonText}
+                    bgColor={slide.bgColor}
+                />
+            ))}
+            
+        </ReactCarousel>
+    );
 };
--- src/libs/utils/carouselData.ts
+++ src/libs/utils/carouselData.ts
@@ -7,6 +7,7 @@ export const carouselData = [
       heading: 'Break the cycle of breakouts',
       subheading: 'Introducing NEW Acne Peel & Acne Peel Plus',
       buttonText: 'Shop Now',
+      bgColor:'bg-gray-100'
     },
     {
       imgSrc: 'https://www.pcaskinpro.com/cms/assets/01200ff0-3aa4-488a-867f-e3747abf19b3.jpg?key=pca-en-us-hero-large',
@@ -14,6 +15,7 @@ export const carouselData = [
       heading: 'Our NEW Hyaluronic Acid Microneedle Eye Patches ',
       subheading: 'Your overnight refresh for under eyes with 22% reduction in wrinkles in just one treatment ',
       buttonText: 'Shop Now',
+      bgColor:'bg-black text-white'
     },
     {
       imgSrc: 'https://www.pcaskinpro.com/cms/assets/201d2cde-cfcb-419b-a2e6-32407713196b.jpg?key=pca-en-us-hero-large',
@@ -21,6 +23,7 @@ export const carouselData = [
       heading: 'Tailor your patients’ regimens ',
       subheading: 'Provide powerful at-home recommendations with PCA SKIN advanced corrective products to dramatically improve skin health.',
       buttonText: 'Shop Now',
+      bgColor:'bg-gray-100'
     },
   ];
   
