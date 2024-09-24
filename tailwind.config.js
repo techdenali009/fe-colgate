ed7c9ffd588f787c78fc4d8a708caf4d5af835f7
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-09-24 17:45:58 +0530

    fonts moved to public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,12 +22,6 @@ module.exports = {
         HeroNewUltraLight:['Hero New UltraLight']
       },
     },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px'
-    }
   },
   plugins: [],
 }
