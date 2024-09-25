c77fbfd5d85aec34c777f7b24c1c2b992497ac96
RamyaJarugulla7 <ramya.jarugulla@techdenali.com>
2024-09-25 19:43:04 +0530

    Modified banner's styling

--- src/libs/ui/src/organisms/PromotionaBannerSection/index.tsx
+++ src/libs/ui/src/organisms/PromotionaBannerSection/index.tsx
@@ -17,15 +17,15 @@ export const PromotionBannerSection: React.FC<PromotionBannerSectionProps> = ({ 
       <PromotionBannerText text={text} heading={heading} paragraph={paragraph} />
       <PromotionBannerList banners={banners} />
       <div className="flex justify-center pt-10 lg:pt-4 ">
-      <SecondaryButton 
-              onClick={() => console.log('Button clicked!')} 
+        <SecondaryButton 
+          onClick={() => console.log('Button clicked!')} 
           className="flex font-HeroNewBold  hover:border-transparent hover:!bg-black bg-[url(src\libs\ui\assests\Arrow-right-blue.495910aa.svg)] hover:bg-[url(src\libs\ui\assests\Right-Icon.e85181bb.svg)] items-center justify-center text-blue-400 border border-blue-500 font-bold px-6 py-2.5 "
-            >
+        >
               Learn More 
-              <div className="ml-2 flex items-center">
-                <Icon icon={arrowIcon} className="h-4 w-4" />
-              </div>
-      </SecondaryButton>
+          <div className="ml-2 flex items-center">
+            <Icon icon={arrowIcon} className="h-4 w-4" />
+          </div>
+        </SecondaryButton>
       </div>
     </div >
   );
