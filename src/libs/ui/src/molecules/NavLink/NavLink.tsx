c3c89e60470a0cc5efe6ef824c13ac971b18647c
swathi <swathi@gmail.com>
2024-09-26 16:24:58 +0530

    eslint issue resolve

--- src/libs/ui/src/molecules/NavLink/NavLink.tsx
+++ src/libs/ui/src/molecules/NavLink/NavLink.tsx
@@ -6,7 +6,7 @@ import { Button } from '@ui/atoms/Button';
 
 const NavLinks: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  // @ts-ignore  activeMenu
+  // @ts-expect-error activeMenu
   const [activeMenu, setActiveMenu] = useState<string | null>(null);
 
   const handleMouseEnter = (title: string) => {
