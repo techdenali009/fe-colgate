f436660efc7597f04e5b2fb3df847a81b03b452d
vanapalli manikanta <manikantavanapalli136@gmail.com>
2024-10-03 14:23:39 +0530

    fixed review commments

--- src/libs/ui/src/molecules/HoveringRatingStar/index.tsx
+++ src/libs/ui/src/molecules/HoveringRatingStar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import StarIcon from '../../../assets/star.svg';
 
 interface StarRatingProps {
   totalStars?: number;
@@ -42,26 +43,7 @@ const StarRating: React.FC<StarRatingProps> = ({ totalStars = 5, initialRating =
               onMouseLeave={handleMouseLeave}
               onClick={() => handleClick(index + 1)}
             >
-              {/* SVG star icon, fill always white for inside of the star */}
-              <svg
-                aria-hidden="true"
-                width="20px"
-                height="20px"
-                fill="none"
-                viewBox="0 0 34 34"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-current stroke-current"
-              >
-                <g stroke="none" fill="none" fillRule="evenodd">
-                  <g fillRule="nonzero" stroke="#125CE0" strokeWidth="1.5">
-                    <path
-                      fill="#ffffff" // Inside of the star is always white
-                      d="M16.3076923,1.76513255 L20.8381599,11.4172719 L31.0592597,12.978917 L23.6603835,20.5621906 L25.3975353,31.2117909 L16.3076923,26.1870493 L7.21784935,31.2117909 L8.95500108,20.5621906 L1.55612493,12.978917 L11.7772247,11.4172719 L16.3076923,1.76513255 Z"
-                    >
-                    </path>
-                  </g>
-                </g>
-              </svg>
+              <img src={StarIcon} alt="Star" width="20" height="20" />
             </span>
           );
         })}
