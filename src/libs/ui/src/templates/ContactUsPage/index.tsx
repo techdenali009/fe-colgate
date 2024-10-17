a2d140fe5cd368a76e21d93fb7df281d493e0e9e
Rohith Malgari <rohith.malgari@gmail.com>
2024-10-17 15:14:21 +0530

    Resolved conflicts.

--- src/libs/ui/src/templates/ContactUsPage/index.tsx
+++ src/libs/ui/src/templates/ContactUsPage/index.tsx
@@ -1,10 +1,10 @@
-import ContactUs from '@ui/organisms/About/ContactUs/ContactUs';
+import ContactUs from '@ui/organisms/About/ContactUs';
 
 function ContactUsTemplate() {
   return (
     <div className="xl:w-[90rem] w-full lg:px-[3.5rem] mx-auto lg:mt-14 mt-5 px-6 pb-7">
     
-      <ContactUs></ContactUs>
+    <ContactUs></ContactUs>
     </div>
   );
 }
