7cacce7e42249f10e22a2ae2221980bcf3f81104
Rohith Malgari <rohith.malgari@gmail.com>
2024-10-17 15:17:18 +0530

    Changes as per eslint.

--- src/libs/ui/src/templates/ContactUsPage/index.tsx
+++ src/libs/ui/src/templates/ContactUsPage/index.tsx
@@ -4,7 +4,7 @@ function ContactUsTemplate() {
   return (
     <div className="xl:w-[90rem] w-full lg:px-[3.5rem] mx-auto lg:mt-14 mt-5 px-6 pb-7">
     
-    <ContactUs></ContactUs>
+      <ContactUs></ContactUs>
     </div>
   );
 }
