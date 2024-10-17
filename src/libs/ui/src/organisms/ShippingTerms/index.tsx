eb10c6648732ac8eff284a9bc4df9ecb8033d6da
Nishant Prabhu <nishant.prabhu@techdenali.com>
2024-10-17 12:55:23 +0530

    separated the reusable CSS class

--- src/libs/ui/src/organisms/ShippingTerms/index.tsx
+++ src/libs/ui/src/organisms/ShippingTerms/index.tsx
@@ -2,7 +2,7 @@ import AnchorLink from '@ui/atoms/AnchorLink'
 import Paragraph from '@ui/atoms/Paragraph/paragraph'
 import Strong from '@ui/atoms/Strong'
 import { HeaderLabel } from '@ui/molecules/HeaderLabel'
-
+import './shippingTerms.style.scss'
 function ShippingTerms() {
   return (
     <div className="flex flex-col items-center">
@@ -20,33 +20,33 @@ function ShippingTerms() {
           <HeaderLabel className="!text-[36px] !p-0 leading-normal font-HeroNewRegular font-normal ">UPS - Shipping Times</HeaderLabel>    
         </div>
         <Paragraph>&nbsp;</Paragraph>
-        <table className="w-[1260.02px]  text-start max-w-full table-fixed  font-SansSerif ">
+        <table className="w-[1260.02px] text-start max-w-full table-fixed  font-SansSerif ">
           <thead className=''>
-            <tr className="bg-black text-white ">
-              <th className="text-left align-bottom py-[1em] px-[15px] tracking-[0.65px] !font-semibold">Shipping Method</th>
-              <th className="text-left align-bottom py-[1em] px-[15px] tracking-[0.4px] !font-semibold">Delivery Time</th>
+            <tr className="bg-appBlackTheme text-white ">
+              <th className="table-heading">Shipping Method</th>
+              <th className="table-heading">Delivery Time</th>
             </tr>
           </thead>
           <tbody className='text-slate-700'>
             <tr>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px] !font-normal ">Standard</span></th>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">3-7 business days</span></th>
+              <th className="table-data"><span className="table-span ">Standard</span></th>
+              <th className="table-data"><span className="table-span">3-7 business days</span></th>
             </tr>
-            <tr className="bg-[#eff2f4]">
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">Express Saver</span></th>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">3 business days</span></th>
-            </tr>
-            <tr className="">
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">2 Day</span></th>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">2 business days</span></th>
-            </tr>
-            <tr className="bg-[#eff2f4]">
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">Standard Overnight</span></th>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">1 business days</span></th>
+            <tr className="table-row-color">
+              <th className="table-data"><span className="table-span">Express Saver</span></th>
+              <th className="table-data"><span className="table-span">3 business days</span></th>
             </tr>
             <tr className="">
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">Priority Overnight</span></th>
-              <th className="text-left align-top py-[1em] px-[15px] text-[1.4rem]"><span className="text-[16px]  !font-normal">Next Day</span></th>
+              <th className="table-data"><span className="table-span">2 Day</span></th>
+              <th className="table-data"><span className="table-span">2 business days</span></th>
+            </tr>
+            <tr className="table-row-color">
+              <th className="table-data"><span className="table-span">Standard Overnight</span></th>
+              <th className="table-data"><span className="table-span">1 business days</span></th>
+            </tr>
+            <tr className="">
+              <th className="table-data"><span className="table-span">Priority Overnight</span></th>
+              <th className="table-data"><span className="table-span">Next Day</span></th>
             </tr>
           </tbody>
         </table>
