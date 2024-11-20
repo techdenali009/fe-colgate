b54af82994bf8e46ed9c231bab91b2ff4782f021
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-11-20 18:08:10 +0530

    verification Ui

---
+++ src/pages/AccountVerificationPage/index.tsx
@@ -0,0 +1,5 @@
+import { AccountVerificationTemplate } from "@ui/templates/AccountVerification";
+
+export const AccountVerificationPage =() =>{
+    return <AccountVerificationTemplate></AccountVerificationTemplate>
+}
--- src/store/services/Endpoints/AuthApi.ts
+++ src/store/services/Endpoints/AuthApi.ts
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const AuthUrl=import.meta.env.VITE_AUTH_URL;
+const AuthUrl = import.meta.env.VITE_AUTH_URL;
 
 export const AuthApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `${AuthUrl}`}),
+  baseQuery: fetchBaseQuery({ baseUrl: `${AuthUrl}` }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (loginData) => ({
@@ -13,11 +13,14 @@ export const AuthApi = createApi({
           'Content-Type': 'application/json',
         },
         body: loginData,
-        
+
       }),
     }),
+    verifyAccount: builder.query({
+      query: (token) => `/users/email/verifyToken?token=${token}`,
+    })
   }),
 });
 
 
-export const { useLoginMutation } = AuthApi;
+export const { useLoginMutation, useLazyVerifyAccountQuery } = AuthApi;
