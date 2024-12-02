9189bff626f4fc7b72178b0b3d7b4d1d9589d1f0
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-12-02 19:21:55 +0530

    toast error

--- src/libs/ui/src/atoms/ProductDetailsPageFavoriteButton/index.tsx
+++ src/libs/ui/src/atoms/ProductDetailsPageFavoriteButton/index.tsx
@@ -48,7 +48,7 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ productId,showClose = f
       const response = await addFavourite(favoriteData).unwrap();
       if (response?.status === 'Success') {
         dispatch(action === 'add' ? addFavorite(productId) : removeFavorite(productId));
-        showSuccessToast(`${action === 'add' ? 'Added to' : 'Removed from'} Favorites`, 'top-right');
+        // showSuccessToast(`${action === 'add' ? 'Added to' : 'Removed from'} Favorites`, 'top-right');
       }
       // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     } catch (err: any) {
